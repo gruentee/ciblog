efe50b8e0611abe765876952798a05a4ee572d0b
Constantin <ckraft@smail.uni-koeln.de>
2015-03-31 22:46:44 +0200

    Added Pages controller and common view files (site header & footer)

---
+++ application/controllers/Pages.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Pages controller
+ * 
+ * @author Constantin Kraft
+ * 
+ */
+ 
+class Pages extends CI_Controller {
+    
+    
+    public function view($page = 'home') {
+        
+    }
+}
