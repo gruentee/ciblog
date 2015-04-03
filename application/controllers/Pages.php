504161d76e0df5e675a812fc896de418a691bfa5
Constantin <ckraft@smail.uni-koeln.de>
2015-04-03 20:06:22 +0200

    Added file header, changed view method according to main template file view method

--- application/controllers/Pages.php
+++ application/controllers/Pages.php
@@ -1,6 +1,11 @@
 <?php
 /**
  * Pages controller
+ * Intended to serve static content from views/pages/ directory
+ * 
+ * Methods that are intented to show something should set 
+ * $this->data['main_content_view'] for the master template to display
+ * in the main content area
  * 
  * @author Constantin Kraft
  * 
@@ -9,20 +14,30 @@
 class Pages extends MY_Controller {
     
     /**
-     * Generic view function
+     * index
+     */
+    public function index() 
+    {
+        $this->view();
+    }
+    
+    /**
+     * View a specific page or default to home page
      * 
      * TODO: documentation
      */
     public function view($page = 'home') 
     {
-        if( ! file_exists( APPPATH . 'views/pages' . $page .'.php' )) 
+        if( ! file_exists( APPPATH . 'views/pages/' . $page .'.php' )) 
         {   
             show_404();
         }
         
-        $this->load->view('templates/header', $data);
-        $this->load->view('pages/'.$page, $data);
-        $this->load->view('templates/footer', $data);
-    
+        //~ $this->load->view('templates/header', $data);
+        //~ $this->load->view('pages/'.$page, $data);
+        //~ $this->load->view('templates/footer', $data);
+        // TODO: maybe create method in MY_Controller
+        $this->data['main_content_view'] = $this->load->view('pages/'.$page, '', TRUE);
+        $this->load->view('default', $this->data);
     }
 }
