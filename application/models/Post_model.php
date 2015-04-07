884c8639e9c91c8003d49d391629c3f2430de0a3
Constantin <ckraft@smail.uni-koeln.de>
2015-04-07 18:45:36 +0200

    Modified get_posts to fetch corresponding user data

--- application/models/Post_model.php
+++ application/models/Post_model.php
@@ -23,11 +23,24 @@ class Post_model extends CI_Model
     {
         if ($slug === FALSE ) 
         {
-            $query = $this->db->get('post');
+            //~ $this->db->select('*');
+            //~ $this->db->from('post');
+            //~ $this->db->join('user', 'user.id=author');
+            // TODO: limit # of posts -> pagination
+            $query = $this->db->get();
+            $this->db->query($sql);
             return $query->result_array();
         }
-        // TODO: sanitize slug, maybe via routing
-        $query = $this->db->get_where('post', array('slug' => $slug));
+        // TODO: validate slug, maybe via routing
+        //~ $query = $this->db->get_where('post', array('slug' => $slug));
+        // TODO: sanitize slug
+        $sql = "SELECT `post`.*, `user`.username, `user`.id
+                FROM `post`
+                INNER JOIN `user` ON `post`.author = `user`.id
+                WHERE `post`.slug = ?
+                AND `post`.active=1";
+        $query = $this->db->query($sql, array($slug));
+        //~ echo($query);
         return $query->row_array();
     }
     
