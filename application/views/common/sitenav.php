314027e2c84dce408bd67c024bc01f75c706ca40
Constantin <ckraft@smail.uni-koeln.de>
2015-04-08 17:01:21 +0200

    Added CodeIgniter Debug Toolbar

---
+++ application/views/common/sitenav.php
@@ -0,0 +1,12 @@
+
+    <div class="blog-masthead">
+      <div class="container">
+        <nav class="blog-nav">
+            <?php 
+                foreach ($sitenav as $link) {
+                    echo $link;
+                }
+            ?>
+        </nav>
+      </div>
+    </div>
