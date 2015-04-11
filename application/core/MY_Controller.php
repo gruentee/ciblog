2277405af5b54c9dfcb533b81e2b8e67cd6cf48b
Constantin <ckraft@smail.uni-koeln.de>
2015-04-11 15:07:10 +0200

    Sidebar content now dynamically generated
    - method _generate_sidebar_content() in MY_Controller generates archive and category links from DB
    - moved view variable setting to MY_Controller constructor
    - added loops in sidebar view

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -17,6 +17,7 @@ class MY_Controller extends CI_Controller {
 
     function __construct() {
         parent::__construct();
+        
         $this->load->helper('html');
         $this->load->helper('form');
         $this->load->helper('url');
@@ -26,7 +27,10 @@ class MY_Controller extends CI_Controller {
         // Load language file
         //~ $this->lang->load('en_admin', 'english');
         
-        $this->_generate_sitenav();
+        $this->data['sitenav'] = $this->_generate_sitenav();
+        
+        // set view variables
+        $this->data['sidebar']  = $this->_generate_sidebar_content();
     }
     
     private function _generate_sitenav()
@@ -46,10 +50,38 @@ class MY_Controller extends CI_Controller {
                 //~ )
             );
         }
-        
-        $this->data['sitenav'] = $navigation;
+        return $navigation;
     }
     
+    private function _generate_sidebar_content()
+    {
+        $this->load->model('category_model');
+        $sidebar = array('archive' => array(), 'categories' => array());
+        // TODO: maybe move HTML generation to views for extra flexibility
+        $cats = $this->category_model->get_categories();
+        foreach ($cats as $cat) 
+        {
+            // TODO: l10n
+            $sidebar['categories'][] = anchor(
+                base_url('category/' . $cat['slug']),
+                $cat['name'],
+                'title="Einträge der Kategorie ' . $cat['name'] . ' zeigen"' 
+            );
+        }
+        // generate link for each month of the year
+        // TODO: l10n
+        for( $i = 1; $i <= 12; $i++ ) {
+            $month_name = strftime( '%B', mktime( 0, 0, 0, $i, 1 ) );
+            $now = now();
+            $link = anchor(
+                base_url(strftime("/%Y/{$i}", $now)),
+                $month_name,
+                'title="Einträge des Monats $monthname anzeigen"'
+            );
+            $sidebar['archive'][] = $link;
+        }
+        return $sidebar;
+    }
 }
 
 ?>
--- application/views/common/sidebar.php
+++ application/views/common/sidebar.php
@@ -8,18 +8,17 @@
         -->
     <h4>Archives</h4>
     <ol class="list-unstyled">
-      <li><a href="#">March 2014</a></li>
-      <li><a href="#">February 2014</a></li>
-      <li><a href="#">January 2014</a></li>
-      <li><a href="#">December 2013</a></li>
-      <li><a href="#">November 2013</a></li>
-      <li><a href="#">October 2013</a></li>
-      <li><a href="#">September 2013</a></li>
-      <li><a href="#">August 2013</a></li>
-      <li><a href="#">July 2013</a></li>
-      <li><a href="#">June 2013</a></li>
-      <li><a href="#">May 2013</a></li>
-      <li><a href="#">April 2013</a></li>
+      <?php foreach ($sidebar['archive'] as $link): ?>
+        <li><?=$link;?></li>
+      <?php endforeach;?>
+    </ol>
+  </div>
+  <h4>Kategorien</h4>
+  <div class="sidebar-module">
+    <ol class="list-unstyled">
+      <?php foreach ($sidebar['categories'] as $link): ?>
+        <li><?=$link;?></li>
+      <?php endforeach;?>
     </ol>
   </div>
   <div class="sidebar-module">
