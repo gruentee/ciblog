65ba80acfc1a2c263c45233b49a26d069f3a52d7
Constantin <ckraft@smail.uni-koeln.de>
2015-04-11 13:50:56 +0200

    Removed alphanumeric restriction from title field in post creation

--- application/config/form_validation.php
+++ application/config/form_validation.php
@@ -11,11 +11,11 @@ $config = array(
         array(
             'field' => 'title',
             'label' => 'Titel',
-            'rules' => 'required|min_length[3]|max_length[60]|alpha_numeric|encode_php_tags',
+            'rules' => 'required|min_length[3]|max_length[60]|encode_php_tags',
             'errors' => array(
                 'required' => '%s darf nicht leer sein',
                 'min_length' => '%s muss zwischen 3 und 60 Zeichen lang sein',
-                'alpha_numeric' => '%s darf nur alphanumerische [A-Z 0..9] Zeichen enthalten.'
+                //~ 'alpha_numeric' => '%s darf nur alphanumerische [A-Z 0..9] Zeichen enthalten.'
             )
         ),
         array(
