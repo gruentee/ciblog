2ee7aaa09bb0bde37bca3e3e616ff6d718b830af
Constantin <ckraft@smail.uni-koeln.de>
2015-04-13 12:32:51 +0200

    Minor view changes
    - put 'no post found' logic in list view
    - changed date to show day # of month

--- application/views/post/list.php
+++ application/views/post/list.php
@@ -1,13 +1,22 @@
-<?php foreach($posts as $post): ?>
+<?php 
+if(count($posts) > 0 ):
+foreach($posts as $post): ?>
 <div class="blog-post">
     <h2 class="blog-post-title">
         <a href="<?=base_url('post/' . $post['slug'])?>"><?php echo $post['title'];?></a>
     </h2>
     <!-- TODO: output author as link to author page 
         -->
-    <p class="blog-post-meta">geschrieben am <?php echo mdate("%D, %m.%Y %h:%i", mysql_to_unix($post['date_created']));?> 
+    <p class="blog-post-meta">geschrieben am <?php echo mdate("%D, %d.%m.%Y %h:%i", mysql_to_unix($post['date_created']));?> 
     von <a href="<?php echo base_url('author/' . $post['user_id']); ?>"><?php echo $post['username']; ?></a></p>
     <?php echo $post['text'];?>
 </div><!-- /.blog-post -->
 
-<?php endforeach; ?>
+<?php endforeach; 
+else:
+?>
+    <p>Keine Posts gefunden.</p>
+<?php
+endif;
+?>
+
--- application/views/post/view.php
+++ application/views/post/view.php
@@ -2,7 +2,7 @@
     <h2 class="blog-post-title"><?php echo $post['title'];?></h2>
     <!-- TODO: localize date 
         -->
-    <p class="blog-post-meta">geschrieben am <?php echo mdate("%D, %m.%Y %h:%i", mysql_to_unix($post['date_created']));?> 
+    <p class="blog-post-meta">geschrieben am <?php echo mdate("%D, %d.%m.%Y %h:%i", mysql_to_unix($post['date_created']));?> 
     von <a href="<?php echo base_url('author/' . $post['user_id']); ?>"><?php echo $post['username']; ?></a></p>
 
     <?php echo $post['text'];?>
