faf44715c9f5e7512b858fad8b822502853653e6
Constantin <ckraft@smail.uni-koeln.de>
2015-04-16 22:21:12 +0200

    Added user model

---
+++ application/models/User_model.php
@@ -0,0 +1,19 @@
+<?php if( ! defined('BASEPATH')) exit ('No direct script access allowed!'); 
+/**
+ * User model
+ * 
+ * User CRUD
+ * 
+ * @author Constantin Kraft
+ * 
+ */
+
+class User_model extends CI_model 
+{
+    public function __construct()
+    {    
+
+    }
+
+}
+
